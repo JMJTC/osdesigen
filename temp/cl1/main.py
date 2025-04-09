59534554b30992f626a6b32f9478cbc39b3e09b3
JMJTC <2801987135@qq.com>
2025-04-09 20:42:13 +0800

    【课设：模拟动态分区存储管理】【ones：7】【改动：优化】

--- temp/cl1/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from memory_simulation import MemorySimulation
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("动态分区管理模拟器")
-    root.geometry("800x600")  # 设置默认窗口大小
-    app = MemorySimulation(root)
-    root.mainloop()
